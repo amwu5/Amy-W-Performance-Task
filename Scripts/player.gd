674d7dd264b7eb417d2677188428b27fd2eab4fa
amwu5 <amwu5@s.sfusd.edu>
2023-12-12 12:23:33 -0800

    sound effects added

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,6 +2,9 @@ extends Area2D
 
 @export var speed = 400
 
+@onready var bg_music = $"../BgMusic"
+@onready var enemy_hit = $"../EnemyHit"
+
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
@@ -19,3 +22,6 @@ func _process(delta):
 
 func _on_body_entered(body):
 	hide()
+	enemy_hit.play()
+	bg_music.play()
+	
